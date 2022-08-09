05c1b14d7e1b6142b1cb4d2afaebd482d43b5343
Thácio Moraes <thacio.moraes@quintoandar.com.br>
2022-08-09 20:52:32 -0300

    add aula 5

---
+++ src/br/com/digitalhouse/aula5/exercicioguiado/Main.java
@@ -0,0 +1,21 @@
+package br.com.digitalhouse.aula5.exercicioguiado;
+
+import br.com.digitalhouse.aula5.exercicioguiado.models.Cliente;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Cliente c1 = new Cliente("Thácio", 1);
+
+    System.out.println("Cliente número " + c1.getNumeroCliente() + " criado!");
+
+    c1.aumentarDivida(100);
+
+    System.out.println(c1.getNome() + ", sua dívida é de " + c1.getDivida());
+
+    c1.pagarDivida(50);
+
+    System.out.println("após pagamento, sua dívida é de " + c1.getDivida());
+  }
+
+}
---
+++ src/br/com/digitalhouse/aula5/exercicioguiado/models/Cliente.java
@@ -0,0 +1,36 @@
+package br.com.digitalhouse.aula5.exercicioguiado.models;
+
+public class Cliente {
+
+  private String nome;
+  private int numeroCliente;
+  private double divida;
+
+  public Cliente(String nome, int numeroCliente) {
+    this.nome = nome;
+    this.numeroCliente = numeroCliente;
+    this.divida = 0;
+  }
+
+  public void aumentarDivida(double valor){
+    this.divida += valor;
+  }
+
+  public void pagarDivida(double valor){
+    if(this.divida - valor >= 0){
+      this.divida -= valor;
+    }
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public int getNumeroCliente() {
+    return numeroCliente;
+  }
+
+  public double getDivida() {
+    return divida;
+  }
+}
